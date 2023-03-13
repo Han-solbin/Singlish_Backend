35b747ec5a92f2a5c59bca3ce64215c794d00eec
Hansolbin <hansolbin@icloud.com>
2023-03-13 23:29:40 +0900

    :tada: Initialize
    Initialize project with spring initializer :
    
    dependency
    (1) Spring boot Web
    (2) Postgres

---
+++ src/test/java/com/example/singlishbackend/SinglishBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.singlishbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglishBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
